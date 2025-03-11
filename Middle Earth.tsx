14472a18527b0b7de1118ae85bc3c54c0fe37ca7
KitHerondaleJr <waves.lahari@gmail.com>
2025-03-12 00:16:09 +0530

    Add files via upload

---
+++ Middle Earth.tsx
@@ -0,0 +1,6 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Middle Earth" tilewidth="32" tileheight="32" tilecount="676" columns="26" backgroundcolor="#6b6b6b" objectalignment="center" tilerendersize="grid" fillmode="preserve-aspect-fit">
+ <tileoffset x="20" y="20"/>
+ <grid orientation="isometric" width="30" height="30"/>
+ <image source="../map1.png" width="848" height="851"/>
+</tileset>
